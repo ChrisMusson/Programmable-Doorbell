bfa00a4544bf6f8997a5987952de701e9eff3582
Chris Musson <chris.musson@hotmail.com>
2020-05-15 13:04:06 +0100

    add pause between each note

--- scripts.js
+++ scripts.js
@@ -35,11 +35,32 @@ var note_lengths = {
 
 function playSong() {
 
-    var AudioContext = window.AudioContext || window.webkitAudioContext;
-    var audioCtx = new AudioContext();
-    var oscillator = audioCtx.createOscillator()
-    oscillator.connect(audioCtx.destination);
-    oscillator.start()
+    // var AudioContext = window.AudioContext || window.webkitAudioContext;
+    // var audioCtx = new AudioContext();
+    // gainNode = audioCtx.createGain();
+    // osc = audioCtx.createOscillator();
+    // osc.connect(gainNode);
+    // gainNode.connect(audioCtx.destination);
+    // osc.type = "sine";
+
+    // // set frequency and gain
+    // osc.frequency.value = 440;
+    // gainNode.gain.value = 1.0;
+    // gainNode.gain.setValueAtTime(0, audioCtx.currentTime + 1);
+    // gainNode.gain.setValueAtTime(1, audioCtx.currentTime + 1.04);
+    // gainNode.gain.setValueAtTime(0, audioCtx.currentTime + 3);
+
+    // // start and connect
+    // osc.start();
+
+    AudioContext = window.AudioContext || window.webkitAudioContext;
+    audioCtx = new AudioContext();
+    gainNode = audioCtx.createGain();
+    oscillator = audioCtx.createOscillator();
+    oscillator.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
+    oscillator.type = "sine";
+    oscillator.start();
 
     var string = document.getElementById("code").value;
     string = string.replace(/\s/g, "");
@@ -50,10 +71,8 @@ function playSong() {
     const y = Math.pow(2, 1 / 12) // frequency multiplier between 2 half notes
     var i;
     var time_elapsed = 0;
-    var time_elapsed = 0;
 
     for (i = 0; i < split_string.length - 1; i++) {
-        console.log(i);
         let input = split_string[i];
         let num_asterisks = input.match(/\*/g) ? input.match(/\*/g).length : 0;
         let note_duration = note_lengths[input[input.length - 1]] / multiplier;
@@ -64,6 +83,14 @@ function playSong() {
 
         oscillator.frequency.setValueAtTime(frequency, time_elapsed);
         time_elapsed += note_duration;
+
+        /* necessary so you can differentiate between consecutive notes of the same frequency
+        not happy with the popping sound it produces though */
+        gainNode.gain.setValueAtTime(0.55, time_elapsed - 0.01);
+        gainNode.gain.setValueAtTime(1, time_elapsed);
+
+        // gainNode.gain.exponentialRampToValueAtTime(0.01, time_elapsed-0.05);
+        // gainNode.gain.exponentialRampToValueAtTime(1, time_elapsed);
     }
 
     oscillator.stop(time_elapsed);
